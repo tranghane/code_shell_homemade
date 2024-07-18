0018a6b1feb8632cf93ab1e9c2a85c2ec2fe3d4f
sameocean <trungduongnguyen2004@gmail.com>
2024-07-17 20:48:51 -0700

    moving new files

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
+[![progress-banner](https://backend.codecrafters.io/progress/shell/1a304fe4-98bf-4ea5-88aa-b93901fb7898)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
+
+This is a starting point for C++ solutions to the
+["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview).
+
+In this challenge, you'll build your own POSIX compliant shell that's capable of
+interpreting shell commands, running external programs and builtin commands like
+cd, pwd, echo and more. Along the way, you'll learn about shell command parsing,
+REPLs, builtin commands, and more.
+
+**Note**: If you're viewing this repo on GitHub, head over to
+[codecrafters.io](https://codecrafters.io) to try the challenge.
+
+# Passing the first stage
+
+The entry point for your `shell` implementation is in `src/main.cpp`. Study and
+uncomment the relevant code, and push your changes to pass the first stage:
+
+```sh
+git add .
+git commit -m "pass 1st stage" # any msg
+git push origin master
+```
+
+Time to move on to the next stage!
+
+# Stage 2 & beyond
+
+Note: This section is for stages 2 and beyond.
+
+1. Ensure you have `cmake` installed locally
+1. Run `./your_shell.sh` to run your program, which is implemented in
+   `src/main.cpp`.
+1. Commit your changes and run `git push origin master` to submit your solution
+   to CodeCrafters. Test output will be streamed to your terminal.
+
+
 # This is a homemade Linux codeshell
 ## Completed task:
 - Handle missing command
@@ -6,7 +43,6 @@
 - Echo builtin
 - Type builtin
 ## How to run program:
-- `cd src`
-- `g++ main.cpp -o my_program`
-- `./my_program`
-- testing
+- make sure to fix the path of your vcpkg in `your_shell.sh`
+- Run `./your_shell.sh` to run your program, which is implemented in
+   `src/main.cpp`.
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,38 @@
 #include <vector>
 #include <string.h>
 #include <map>
+#include <sstream>
+#include <filesystem>
 using namespace std;
+
+// check path from name
+std::string get_path(std::string fileName)
+{
+  // get the environment variable PATH
+  std::string path_env = std::getenv("PATH");
+
+  // convert it to a stringstream
+  std::stringstream ss(path_env);
+
+  // variable to store path and use for getline
+  std::string path;
+
+  // go over the stringstream, aka the environment variable PATH
+  while (!ss.eof())
+  {
+    // for each path, create an abs_path
+    getline(ss, path, ':');
+    string abs_path = path + '/' + fileName;
+
+    // use abs_path to check if the file exist
+    if (filesystem::exists(abs_path))
+    {
+      return abs_path;
+    }
+  }
+  // if find nothing return
+  return "";
+}
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -14,24 +45,22 @@ int main()
 
   // Uncomment this block to pass the first stage
   std::map<string, string> commandToType = {
-    {"echo", "a shell builtin"},
-    {"exit", "a shell builtin"},
-    {"cat", "/bin/cat"},
-    {"type", "a shell builtin"}
-  };
+      {"echo", "a shell builtin"},
+      {"exit", "a shell builtin"},
+      {"cat", "/bin/cat"},
+      {"type", "a shell builtin"}};
   std::cout << "$ ";
   while (true)
   {
-    
+
     std::string input;
     std::getline(std::cin, input);
 
-
-    if (input.find("exit") == 0 && input[5] == '0' && input.length() == 6) //exit 0 builtin
+    if (input.find("exit") == 0 && input[5] == '0' && input.length() == 6) // exit 0 builtin
     {
       break;
     }
-    else if (input.find("echo") == 0) //echo builtin
+    else if (input.find("echo") == 0) // echo builtin
     {
       // for (int i = 5; i < input.length(); i++)
       // {
@@ -42,15 +71,27 @@ int main()
       std::cout << text << std::endl;
       std::cout << "$ ";
     }
-    else if (input.find("type") == 0) { //type builtin
+    else if (input.find("type") == 0)
+    { // type builtin
       const int TYPE_LEN = 5;
       std::string text = input.substr(TYPE_LEN);
-      if (commandToType.find(text) == commandToType.end()) { //if can't find
+
+      if (commandToType.find(text) == commandToType.end())
+      { // if can't find
         std::cout << text + ": not found" << std::endl;
         std::cout << "$ ";
-      } else {
-        std::cout << text + " is " + commandToType[text]<< std::endl;
-        std::cout << "$ ";
+      }
+      else
+      {
+        std::string path = get_path(input);
+        if (path.empty())
+        {
+          std::cout << input << " not found\n";
+        }
+        else
+        {
+          std::cout << input << " is " << path << std::endl;
+        }
       }
     }
     else
